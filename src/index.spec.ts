2d4a95da504844d3c976cf34c16893308934a2e6
Michael Zandstra <michael@tunnel.ninja>
2020-04-20 19:52:02 -0600

    remove broken test

--- src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as F from '.'
-
-describe('F', () => {
-  it('renameKeys', () => {
-    const obj = { x: 1 }
-    const map = { x: 'y' }
-    expect(F.renameKeys(map, obj)).toEqual({ y: 1 })
-  })
-})
