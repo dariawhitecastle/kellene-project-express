fc3bc89b171722e71d81c5116907bcf471deaefb
Michael Zandstra <michael@tunnel.ninja>
2020-04-20 19:51:28 -0600

    fix tests

--- src/typeorm.config.spec.ts
+++ src/typeorm.config.spec.ts
@@ -2,6 +2,6 @@ import ormconfig from './typeorm.config'
 
 describe('ormconfig', () => {
   it('should have the correct db name', () => {
-    expect(ormconfig.database).toBe('express_typeorm')
+    expect(ormconfig.database).toBe('forensic_truth')
   })
 })
